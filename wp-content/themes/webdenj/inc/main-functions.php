bf2d996053b7feb499ff6e2bd513e8da4c10cad9
MrDEATH102 <massamongus@gmail.com>
2025-06-05 15:24:25 +0330

    added fonts

--- wp-content/themes/webdenj/inc/main-functions.php
+++ wp-content/themes/webdenj/inc/main-functions.php
@@ -4,6 +4,6 @@ function webdenj_style_script(){
 
     //enqueue styles
     wp_enqueue_style( 'Bootstrap', get_template_directory_uri().'/assets/css/bootstrap-grid.min.css', array(), '5.3.6', 'all');
-    wp_enqueue_style( 'main-style', get_template_directory_uri().'/assets/css/main-style.css', array('bootstrap'), '1.0.0', 'all');
+    wp_enqueue_style( 'main-style', get_template_directory_uri().'/assets/css/main-style.css', array('Bootstrap'), '1.0.0', 'all');
 }
 add_action('wp_enqueue_scripts', 'webdenj_style_script');
