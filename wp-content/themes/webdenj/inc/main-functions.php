70206c1e23537fde372d2ba67539053d052a67ec
MrDEATH102 <massamongus@gmail.com>
2025-06-05 15:44:45 +0330

    enqueu scripts

--- wp-content/themes/webdenj/inc/main-functions.php
+++ wp-content/themes/webdenj/inc/main-functions.php
@@ -5,5 +5,8 @@ function webdenj_style_script(){
     //enqueue styles
     wp_enqueue_style( 'Bootstrap', get_template_directory_uri().'/assets/css/bootstrap-grid.min.css', array(), '5.3.6', 'all');
     wp_enqueue_style( 'main-style', get_template_directory_uri().'/assets/css/main-style.css', array('Bootstrap'), '1.0.0', 'all');
+
+    //enqueue scripts
+    wp_enqueue_script( 'main', get_template_directory_uri().'/assets/js/main.js', array('jquery'), '1.0.0', true );
 }
 add_action('wp_enqueue_scripts', 'webdenj_style_script');
