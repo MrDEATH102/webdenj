f2a10866681723ab28e62c452d73a5e69bf23e29
MrDEATH102 <massamongus@gmail.com>
2025-06-06 13:49:01 +0330

    course sub menu

--- wp-content/themes/webdenj/inc/main-functions.php
+++ wp-content/themes/webdenj/inc/main-functions.php
@@ -4,6 +4,7 @@ function webdenj_style_script(){
 
     //enqueue styles
     wp_enqueue_style( 'Bootstrap', get_template_directory_uri().'/assets/css/bootstrap-grid.min.css', array(), '5.3.6', 'all');
+    wp_enqueue_style('fontawesome', get_template_directory_uri().'/assets/css/all.min.css', array(), '6.1.2','all');
     wp_enqueue_style( 'main-style', get_template_directory_uri().'/assets/css/main-style.css', array('Bootstrap'), '1.0.0', 'all');
 
     //enqueue scripts
