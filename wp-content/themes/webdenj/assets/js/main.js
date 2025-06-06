ebcd3309467cebec03892c055afe652ae6385917
MrDEATH102 <massamongus@gmail.com>
2025-06-06 16:44:44 +0330

    overlay section make

---
+++ wp-content/themes/webdenj/assets/js/main.js
@@ -0,0 +1,9 @@
+// overlay show function
+function overlay_section(){
+    jQuery('.overlay__section').addClass('active');
+}
+
+// overlay hide fuction
+function overlay_section_hide(){
+    jQuery('.overlay__section').removeClass('active');
+}
--- wp-content/themes/webdenj/footer.php
+++ wp-content/themes/webdenj/footer.php
@@ -1,4 +1,12 @@
-
+<!-- start widgets items -->
+<!-- search widgets  -->
+<div class="search__widget">
+</div>
+<!-- End widget Items  -->
+<!-- start overlay section -->
+ <div onclick="overlay_section_hide()" class="overlay__section"></div>
+<!-- end overlay section -->
 <?php wp_footer(); ?>
 </body>
+
 </html>
