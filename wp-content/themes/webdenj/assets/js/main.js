0bcb817171404bf94a97467576f067ae8df89766
MrDEATH102 <massamongus@gmail.com>
2025-06-07 00:15:23 +0330

    dark mode set

--- wp-content/themes/webdenj/assets/js/main.js
+++ wp-content/themes/webdenj/assets/js/main.js
@@ -1,6 +1,20 @@
 jQuery(document).ready(function ($) {
 
 
+    // check theme mode 
+    if (!localStorage.getItem('theme-mode')) {
+        localStorage.setItem('theme-mode', 'light');
+        $('body').attr('data-mode', 'light');
+    } else {
+        localStorage.setItem('theme-mode', localStorage.getItem('theme-mode'));
+        $('body').attr('data-mode', localStorage.getItem('theme-mode'));
+        if(localStorage.getItem('theme-mode')=='light'){
+            $('.cta__header__right--mode').addClass('active');
+        }else{
+            $('.cta__header__right--mode').removeClass('active');
+        }
+    }
+
     // show and hide search popup
     $('.cta__header__right--search').click(function () {
         // open overlay
@@ -18,6 +32,20 @@ jQuery(document).ready(function ($) {
         $('.search__widget__result').html('');
         $('#search__widget__input').val('');
         overlay_section_hide();
+    })
+    //set theme mode
+    $('.cta__header__right--mode').on('click', function (e) {
+        e.preventDefault();
+        let themeMode = localStorage.getItem('theme-mode');
+        if (themeMode == 'light') {
+            $('body').attr('data-mode', 'dark');
+            localStorage.setItem('theme-mode', 'dark');
+            $(this).removeClass('active');
+        } else {
+            $('body').attr('data-mode', 'light');
+            localStorage.setItem('theme-mode', 'light');
+            $(this).addClass('active');
+        }
     })
 })
 
