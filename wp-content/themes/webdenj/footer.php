f85bfe2ef328afa07a1519e2f05a34b35c7563d2
MrDEATH102 <massamongus@gmail.com>
2025-06-07 15:18:23 +0330

    swipper

--- wp-content/themes/webdenj/footer.php
+++ wp-content/themes/webdenj/footer.php
@@ -1,11 +1,10 @@
-<!-- start widgets items -->
-<!-- search widgets  -->
-    <?php get_template_part('widgets/search', 'widget') ?>
-<!-- End widget Items  -->
-<!-- start overlay section -->
- <div onclick="overlay_section_hide()" class="overlay__section"></div>
-<!-- end overlay section -->
-<?php wp_footer(); ?>
+<!-- Start Widgets Items -->
+    <!-- Search Widgets -->
+    <?php get_template_part('widgets/search','widget');?>
+<!-- End Widgets Items -->
+<!-- Start Overlay section -->
+    <div onclick="Overlay_section_hide()" class="overlay__section"></div>
+<!-- End Overlay section -->
+<?php wp_footer();?>
 </body>
-
 </html>
--- wp-content/themes/webdenj/header.php
+++ wp-content/themes/webdenj/header.php
@@ -1,32 +1,33 @@
 <!DOCTYPE html>
-<html <?php language_attributes() ?>>
-
+<html <?php language_attributes(); ?>>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo('name') ?></title>
-
-    <?php wp_head(); ?>
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head();?>
 </head>
-
 <body>
-    <!-- start header section  -->
+    <!-- Start header section -->
 
-    <header class='main_header'>
+    <header class="main_header">
         <div class="container">
             <div class="row">
-                <!-- Header logo component  -->
+                <!-- Header logo component -->
                 <div class="col-lg-2 col-12">
-                    <?php get_template_part('templates/header/logo', 'component'); ?>
+                    <?php get_template_part('templates/header/logo','component'); ?>
                 </div>
-                <!-- Header menu component  -->
+                <!-- Header menu component -->
                 <div class="col-lg-7 col-12">
-                    <?php get_template_part('templates/header/menu', 'component'); ?>
+                    <?php get_template_part('templates/header/menu','component'); ?>
                 </div>
-                <!-- Header CTA component  -->
+                <!-- Header CTA component -->
                 <div class="col-lg-3 col-12">
-                    <?php get_template_part('templates/header/cta', 'component') ?>
+                    <?php get_template_part('templates/header/cta','component');?>
                 </div>
             </div>
         </div>
     </header>
+
+    <!-- End header section -->
+    
+    
